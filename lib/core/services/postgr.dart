9009a361ab7446012b6f663446a26b48dc935374
Arshad ullah <jahan665577@gmail.com>
2026-06-29 09:52:56 +0500

    working on postgres db

---
+++ lib/core/services/postgr.dart
@@ -0,0 +1,21 @@
+import 'package:postgres/postgres.dart';
+
+class DatabaseService {
+  static Connection? _connection;
+
+  static Future<Connection> connect() async {
+    if (_connection != null) return _connection!;
+
+    _connection = await Connection.open(
+      Endpoint(
+        host: 'localhost',
+        port: 5432,
+        database: "students",
+        username: "MAC",
+        password: "1234",
+      ),
+    );
+
+    return _connection!;
+  }
+}
